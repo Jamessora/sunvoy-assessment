95ba3c75eb81c997c0959c3ca53c307b6ff77a9f
Jamessora <cadaciojerico@example.com>
2025-06-12 17:30:48 +0800

    Add fetch users function.

--- src/index.ts
+++ src/index.ts
@@ -62,8 +62,24 @@ async function login(): Promise<string> {
   return formattedAuthCookie;
 }
 
+async function fetchUsers(cookie: string): Promise<any[]> {
+  console.log('Fetching all users');
+  const response = await fetch('https://challenge.sunvoy.com/api/users', {
+    method: 'POST',
+    headers: { 
+      Cookie: cookie, 
+      Accept: 'application/json',
+    },
+  });
+  if (!response.ok) throw new Error(`Users fetch failed (${response.status})`);
+  const users = await response.json();
+  console.log(`Retrieved ${users.length} users`);
+  return users;
+}
+
 async function main() {
   const cookie = await login();
+  const users = await fetchUsers(cookie);
 }
 
 main().catch(err => {
